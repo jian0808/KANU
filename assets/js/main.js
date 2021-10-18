9a7414854d47a6a086a642c0ca4b553ee05b19ef
jian0808 <90013630+jian0808@users.noreply.github.com>
2021-10-18 20:13:07 +0900

    20211018

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ $('.tab02').click(function(){
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerGroup: 2,
+    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,        
     pagination: {
@@ -32,6 +32,16 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: { 
+        280: { 
+            slidesPerView: 2, 
+            spaceBetween: 20 
+        }, 
+        1024: { 
+            slidesPerView: 4,
+            spaceBetween: 30 
+        } 
+    }
 });
 
 // section05
